b5da0e9ed3e15027477e7edf8949ec15f7756d3d
Bernie Telles <btelles@gmail.com>
2012-03-27 10:54:37 -0700

    Initial commit (empty generated rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WildfirePc::Application.config.secret_token = '1ce17473160c4daae198991009fed89aa67e32e5b7e0185297bc1bc31ae6c7346fb6674b4c3f28d56aae865470b5d2d3b40be16c263a920184f0b590874ac059'
