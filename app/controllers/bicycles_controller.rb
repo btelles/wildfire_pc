7465c29be0af46562bc7d8b8f804f58fe221072e
Bernie Telles <btelles@gmail.com>
2012-03-27 16:29:25 -0700

    [EPIC B] Add crud functionality to bicycles.

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,83 +1,19 @@
 class BicyclesController < ApplicationController
-  # GET /bicycles
-  # GET /bicycles.json
-  def index
-    @bicycles = Bicycle.all
+  expose :bicycle
+  expose(:bicycles) { Bicycle.scoped }
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @bicycles }
-    end
-  end
-
-  # GET /bicycles/1
-  # GET /bicycles/1.json
-  def show
-    @bicycle = Bicycle.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @bicycle }
-    end
-  end
-
-  # GET /bicycles/new
-  # GET /bicycles/new.json
-  def new
-    @bicycle = Bicycle.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @bicycle }
-    end
-  end
-
-  # GET /bicycles/1/edit
-  def edit
-    @bicycle = Bicycle.find(params[:id])
-  end
-
-  # POST /bicycles
-  # POST /bicycles.json
   def create
-    @bicycle = Bicycle.new(params[:bicycle])
-
-    respond_to do |format|
-      if @bicycle.save
-        format.html { redirect_to @bicycle, notice: 'Bicycle was successfully created.' }
-        format.json { render json: @bicycle, status: :created, location: @bicycle }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @bicycle.errors, status: :unprocessable_entity }
-      end
+    if bicycle.save
+      redirect_to bicycle, notice: 'Bicycle was successfully saved.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /bicycles/1
-  # PUT /bicycles/1.json
-  def update
-    @bicycle = Bicycle.find(params[:id])
+  alias update create
 
-    respond_to do |format|
-      if @bicycle.update_attributes(params[:bicycle])
-        format.html { redirect_to @bicycle, notice: 'Bicycle was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @bicycle.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bicycles/1
-  # DELETE /bicycles/1.json
   def destroy
-    @bicycle = Bicycle.find(params[:id])
-    @bicycle.destroy
-
-    respond_to do |format|
-      format.html { redirect_to bicycles_url }
-      format.json { head :no_content }
-    end
+    bicycle.destroy
+    redirect_to bicycles_url
   end
 end
--- spec/requests/bicycles_spec.rb
+++ spec/requests/bicycles_spec.rb
@@ -1,13 +1,50 @@
 require 'spec_helper'
 
 describe "Bicycles" do
-  describe "GET /bicycles" do
-    let(:bicycle) { FactoryGirl.create(:bicycle, :name => 'First bike') }
+  let(:bicycle) { FactoryGirl.create(:bicycle, :name => 'First bike') }
 
+  describe "GET /bicycles" do
     it "lists all bicycles" do
       bicycle
-      get bicycles_path
-      response.body.should match /First bike/
+      visit bicycles_path
+      page.should have_css 'td', :text => 'First bike'
+    end
+  end
+
+  describe "#show" do
+    it "shows a bicycle" do
+      visit bicycle_path(bicycle)
+      page.should have_css 'p', text: 'First bike'
+    end
+  end
+
+  describe "#new" do
+    it "lets you create new bicycles" do
+      visit new_bicycle_path
+      fill_in('Name', with: 'My first bike')
+      click_button 'Save'
+      page.should have_css('#notice', 'Bicycle was successfully saved')
+    end
+  end
+
+  describe "#edit" do
+    it "lets you change existing bicycles" do
+      visit edit_bicycle_path(bicycle)
+      fill_in('Name', with: 'My second bike')
+      click_button 'Save'
+      page.should have_css('#notice', 'Bicycle was successfully saved')
+      page.save_and_open_page
+      page.should have_css('p', :text => 'My second bike')
+    end
+  end
+
+  describe "#destroy", js: true do
+    it "lets you destroy/remove an existing bicycle" do
+      bicycle
+      visit bicycles_path
+      page.evaluate_script('window.confirm = function() { return true; }')
+      click_link 'Destroy'
+      page.should_not have_css('td', 'My first bike')
     end
   end
 end
